fdf67ccdf8eef0f06d1e8caf8048de86dedd6c56
JOurspectre <93581999+JOurspectre@users.noreply.github.com>
2025-11-21 18:01:40 +0800

    test1

---
+++ data.js
@@ -0,0 +1,337 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-dine-in",
+      "name": "Dine-In",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1472,
+      "initialViewParameters": {
+        "yaw": 1.2698014284920074,
+        "pitch": 0.23212757295075193,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.369780559515128,
+          "pitch": 0.21612150901094473,
+          "rotation": 10.995574287564278,
+          "target": "1-kitchen-a"
+        },
+        {
+          "yaw": 1.5881494835147727,
+          "pitch": 0.35157757144085267,
+          "rotation": 1.5707963267948966,
+          "target": "3-counter-dinein"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.697208920248098,
+          "pitch": -0.2421789550407869,
+          "title": "CCTV DineIn",
+          "text": "<span style=\"font-size: 16px; background-color: rgba(103, 115, 131, 0.8);\">CCTV DineIn</span>"
+        },
+        {
+          "yaw": -0.3980668960937983,
+          "pitch": -0.28398230692856075,
+          "title": "CCTV DineIn",
+          "text": "<span style=\"font-size: 16px; background-color: rgba(103, 115, 131, 0.8);\">CCTV DineIn</span>"
+        },
+        {
+          "yaw": 2.879759471229489,
+          "pitch": -0.301797361097373,
+          "title": "CCTV DineIn",
+          "text": "<span style=\"font-size: 16px; background-color: rgba(103, 115, 131, 0.8);\">CCTV DineIn</span>"
+        },
+        {
+          "yaw": 2.023028257613258,
+          "pitch": -0.20332836789423858,
+          "title": "CCTV 4",
+          "text": "Counter A"
+        },
+        {
+          "yaw": -1.1769844311464066,
+          "pitch": -0.21123724639674357,
+          "title": "CCTV DineIn",
+          "text": "<span style=\"font-size: 16px; background-color: rgba(103, 115, 131, 0.8);\">CCTV DineIn</span>"
+        },
+        {
+          "yaw": -2.6315284463562154,
+          "pitch": -0.1840153613374511,
+          "title": "CCTV DineIn",
+          "text": "<span style=\"font-size: 16px; background-color: rgba(103, 115, 131, 0.8);\">CCTV DineIn</span>"
+        },
+        {
+          "yaw": -1.5398569238461786,
+          "pitch": -0.14288841495375948,
+          "title": "CCTV Outside",
+          "text": "<span style=\"font-size: 16px; background-color: rgba(103, 115, 131, 0.8);\">CCTV Outside</span>"
+        },
+        {
+          "yaw": -1.4484496054763625,
+          "pitch": -0.13916223984303855,
+          "title": "CCTV DineIn",
+          "text": "<span style=\"font-size: 16px; background-color: rgba(103, 115, 131, 0.8);\">CCTV DineIn</span>"
+        },
+        {
+          "yaw": 0.5735379355455343,
+          "pitch": -0.2084416305933452,
+          "title": "CCTV Outside",
+          "text": "<span style=\"font-size: 16px; background-color: rgba(103, 115, 131, 0.8);\">CCTV Outside</span>"
+        },
+        {
+          "yaw": 2.238301665530747,
+          "pitch": -0.15589807014716328,
+          "title": "CCTV Outside",
+          "text": "<span style=\"font-size: 16px; background-color: rgba(103, 115, 131, 0.8);\">CCTV Outside</span>"
+        }
+      ]
+    },
+    {
+      "id": "1-kitchen-a",
+      "name": "Kitchen-A",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1472,
+      "initialViewParameters": {
+        "yaw": -0.3390455023029393,
+        "pitch": 0.31990492902841083,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5252815475765544,
+          "pitch": 0.48102368961737163,
+          "rotation": 0,
+          "target": "0-dine-in"
+        },
+        {
+          "yaw": 1.556592607485638,
+          "pitch": 0.5156978808901567,
+          "rotation": 5.497787143782138,
+          "target": "2-kitchen-b"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.3476805376179719,
+          "pitch": -0.6070428653760747,
+          "title": "Cabinet of Amplifier &amp; CCTV, NVR",
+          "text": "<span style=\"font-size: 16px; background-color: rgba(103, 115, 131, 0.8);\">Cabinet of Amplifier &amp; CCTV, NVR</span>"
+        },
+        {
+          "yaw": 2.257511670446,
+          "pitch": -0.4800382727551842,
+          "title": "CCTV",
+          "text": "<span style=\"font-size: 16px; background-color: rgba(103, 115, 131, 0.8);\">CCTV</span>"
+        },
+        {
+          "yaw": 2.633165044943887,
+          "pitch": -0.4780425773564154,
+          "title": "CCTV",
+          "text": "<span style=\"font-size: 16px; background-color: rgba(103, 115, 131, 0.8);\">CCTV</span>"
+        },
+        {
+          "yaw": -0.5427326196627238,
+          "pitch": -0.6354373872322103,
+          "title": "CCTV",
+          "text": "<span style=\"font-size: 16px; background-color: rgba(103, 115, 131, 0.8);\">CCTV</span>"
+        }
+      ]
+    },
+    {
+      "id": "2-kitchen-b",
+      "name": "Kitchen-B",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1472,
+      "initialViewParameters": {
+        "yaw": -1.0003336175710658,
+        "pitch": 0.3034701189711946,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.468363312880956,
+          "pitch": 0.9858734494241226,
+          "rotation": 0,
+          "target": "1-kitchen-a"
+        },
+        {
+          "yaw": -1.3514092845635197,
+          "pitch": 0.4366299994432943,
+          "rotation": 0,
+          "target": "4-counter-takeout"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.3465450936534928,
+          "pitch": -0.21438589022921306,
+          "title": "CCTV Kitchen B",
+          "text": "<span style=\"font-size: 16px; background-color: rgba(103, 115, 131, 0.8);\">CCTV Kitchen B</span>"
+        }
+      ]
+    },
+    {
+      "id": "3-counter-dinein",
+      "name": "Counter-DineIn",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1472,
+      "initialViewParameters": {
+        "yaw": -1.7575459482931173,
+        "pitch": 0.3082325952256433,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.08806811633339429,
+          "pitch": 0.6095697720882551,
+          "rotation": 0,
+          "target": "0-dine-in"
+        },
+        {
+          "yaw": 2.9912514789715736,
+          "pitch": 0.7987896260128728,
+          "rotation": 0,
+          "target": "4-counter-takeout"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.8575749556337673,
+          "pitch": -0.3362542804308717,
+          "title": "CCTV Counter DineIn",
+          "text": "<span style=\"font-size: 16px; background-color: rgba(103, 115, 131, 0.8);\">CCTV Counter DineIn</span>"
+        },
+        {
+          "yaw": -2.6461456017108134,
+          "pitch": -0.2515792232703298,
+          "title": "CCTV Counter TakeOut",
+          "text": "<span style=\"font-size: 16px; background-color: rgba(103, 115, 131, 0.8);\">CCTV Counter TakeOut</span>"
+        }
+      ]
+    },
+    {
+      "id": "4-counter-takeout",
+      "name": "Counter-TakeOut",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1472,
+      "initialViewParameters": {
+        "yaw": -0.6302763460773786,
+        "pitch": 0.10674579033048914,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.398403226679771,
+          "pitch": 0.6303336808551325,
+          "rotation": 0,
+          "target": "2-kitchen-b"
+        },
+        {
+          "yaw": -1.9136390731707777,
+          "pitch": 0.9631128782943161,
+          "rotation": 0,
+          "target": "3-counter-dinein"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "PITX CCTV Location",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
